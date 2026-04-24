025531bc63cbaed4ff99ef2f85e9dce05b331826
crayze <vityasuhih@gmail.com>
2026-04-24 14:07:59 +0300

    Добавил инфу о диске

--- hello.sh
+++ hello.sh
@@ -6,5 +6,6 @@ echo "Сегодня: $(date)"
 echo "Время аптайма: $(uptime -p)"
 echo "Свободно места: $(df -h / | tail -1 | awk '{print $4}')"
 echo "Мой IP: $(hostname -I | awk '{print $1}')"
+echo "Диск /: занято $(df -h / | tail -1 | awk '{print $3}') из $(df -h / | tail -1 | awk '{print $2}')"
 echo "==================================="
 
