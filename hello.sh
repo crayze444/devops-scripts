460d07514df4b8b7de25512db68d3479a50428dd
crayze <vityasuhih@gmail.com>
2026-04-24 14:10:41 +0300

    Добавил аптайм в main

--- hello.sh
+++ hello.sh
@@ -6,5 +6,6 @@ echo "Сегодня: $(date)"
 echo "Время аптайма: $(uptime -p)"
 echo "Свободно места: $(df -h / | tail -1 | awk '{print $4}')"
 echo "Мой IP: $(hostname -I | awk '{print $1}')"
+echo "Аптайм: $(uptime -p)"
 echo "==================================="
 
